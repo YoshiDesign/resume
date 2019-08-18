e0522df0cef9ec4d8423bb43fc55acb71e230ea6
YoshiDesign <yoshidesigns@outlook.com>
2019-08-18 11:25:48 -0400

    marketing

--- src/app/detail-window/detail-window.component.ts
+++ src/app/detail-window/detail-window.component.ts
@@ -1,24 +1,62 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-detail-window',
-  templateUrl: './detail-window.component.html',
-  styleUrls: ['./detail-window.component.scss']
+selector: 'app-detail-window',
+templateUrl: './detail-window.component.html',
+styleUrls: ['./detail-window.component.scss']
 })
 export class DetailWindowComponent implements OnInit {
 
-  public projectDetails : HTMLElement;
-  public certifiDetails : HTMLElement;
-  public resumeDetails  : HTMLElement;
+    public projectDetails : HTMLElement;
+    public certifiDetails : HTMLElement;
+    public resumeDetails  : HTMLElement;
+    public pHead : HTMLSpanElement;
+    public cHead : HTMLSpanElement;
+    public rHead : HTMLSpanElement;
 
-  constructor() { }
+    public pHide : Boolean;
+    public cHide : Boolean;
+    public rHide : Boolean;
 
-  ngOnInit() {
-    this.projectDetails = document.getElementById('projects');
-    this.certifiDetails = document.getElementById('certs');
-    this.resumeDetails  = document.getElementById('resume');
-  }
+    constructor() { }
+
+    ngOnInit() {
+
+        // Setups
+        this.pHide = false;
+        this.cHide = true;
+        this.rHide = true;
+
+        this.resumeDetails = document.getElementById('resume');
+
+    }
+
+    /* *
+    *   Display F'ns
+    * */
+    showProjects () : void  {
+
+        this.resumeDetails.scrollTo(0,0);
+        this.pHide = false;
+        this.cHide = true;
+        this.rHide = true;
+    }
+
+    showCerts () : void  {
+
+        this.resumeDetails.scrollTo(0,0);
+        this.pHide = true;
+        this.cHide = false;
+        this.rHide = true;
+    }
+
+    showResume () : void  {
+
+        this.pHide = true;  
+        this.cHide = true;
+        this.rHide = false;
 
 
+    }
 
 }
