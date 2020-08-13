7c5973e78c2c9f5e289812d7c9c72eddd94794d1
YoshiDesign <yoshidesigns@outlook.com>
2020-08-13 18:32:17 -0400

    added badges

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SocialViewComponent } from './social-view/social-view.component';
+import { ProjectViewComponent } from './project-view/project-view.component';
+import { BadgeComponentComponent } from './badge-component/badge-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SocialViewComponent,
+    ProjectViewComponent,
+    BadgeComponentComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/social-view/social-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-social-view',
+  templateUrl: './social-view.component.html',
+  styleUrls: ['./social-view.component.css']
+})
+export class SocialViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
