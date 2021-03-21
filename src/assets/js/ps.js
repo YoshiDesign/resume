1a26b03e9dffb00b69399af1537b7d5a50fb0239
YoshiDesign <yoshidesigns@outlook.com>
2021-03-21 11:41:39 -0400

    updating site

---
+++ src/assets/js/ps.js
@@ -0,0 +1,11 @@
+<div id="pong" class="hide-me">
+    <div id="paddle-1" style="top: 54%;">
+
+    </div>
+    <div id="paddle-2" style="top: 54%;">
+
+    </div>
+    <div id="ball" style="left: 45%; top: 56%;">
+
+    </div>
+</div>
